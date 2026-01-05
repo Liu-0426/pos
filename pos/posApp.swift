fc31789d451e9637d4ae09be57c3170f87b6d1b9
Paul <paulliu00426@gmail.com>
2026-01-05 12:20:06 +0800

    Initial Commit

---
+++ pos/posApp.swift
@@ -0,0 +1,17 @@
+//
+//  posApp.swift
+//  pos
+//
+//  Created by 紫寧 on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct posApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
